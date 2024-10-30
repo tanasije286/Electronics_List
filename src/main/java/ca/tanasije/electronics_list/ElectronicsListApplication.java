a897b0ea0a717fa7e6b9c2b818eceab526bba954
Marko Tanasijevic <tanasije286@gmail.ca>
2024-10-30 10:04:21 -0400

    Initial Commit

---
+++ src/main/java/ca/tanasije/electronics_list/ElectronicsListApplication.java
@@ -0,0 +1,13 @@
+package ca.tanasije.electronics_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tanasije/electronics_list/ElectronicsListApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tanasije.electronics_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
