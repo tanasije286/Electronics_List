d2d9d7b5d6f19516508a06689d5f24ed7c5cb40d
tanasije286 <tanasije286@gmail.com>
2024-10-30 10:53:10 -0400

    Initial create for location enum

---
+++ src/main/java/ca/tanasije/electronics_list/models/enums/location.java
@@ -0,0 +1,27 @@
+package ca.tanasije.electronics_list.models.enums;
+
+public enum location {
+    ROOM_200,
+    ROOM_205,
+    ROOM_209,
+    ROOM_215,
+    ROOM_215_STORE_ROOM,
+    ROOM_215_WARDROBE(),
+    ROOM_217,
+    ADVISORY,
+    AMPLUS,
+    AYOS,
+    ROOM_B9,
+    CALGARY,
+    COMPUTER_LAB,
+    FINANCE,
+    FRONT_DESK,
+    HYBRID,
+    KNURLING,
+    MOKAL,
+    LAB_A,
+    LAB_B,
+    MONTREAL,
+    NOT_APPLICABLE,
+    RICHMOND_HILL
+}
