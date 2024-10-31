337e0c98eff87a807d61f6e24ec7d8986f24a446
tanasije286 <tanasije286@gmail.com>
2024-10-31 14:38:54 -0400

    Initial create ErrorCode.java

--- src/main/java/ca/tanasije/electronics_list/exception/error_code/ErrorCode.java
+++ src/main/java/ca/tanasije/electronics_list/exception/error_code/ErrorCode.java
@@ -1,6 +1,7 @@
 package ca.tanasije.electronics_list.exception.error_code;
 
 public class ErrorCode {
-    private Long statusCode;
-    private String message;
+    public static final String ERROR_LAPTOP_NOT_FOUND = "Laptop does not exist with id: ";
+    public static final String ERROR_NO_HANDLER_MAPPING = "Resource not found: ";
+    public static final String ERROR_INTERNAL_SERVER_ERROR = "Internal Server Error";
 }
