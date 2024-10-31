40b9c62d9190ef812e5424bb08c981f9a702c9b0
tanasije286 <tanasije286@gmail.com>
2024-10-31 14:30:30 -0400

    Initial Create for NoSuchLaptopExistsException.java

---
+++ src/main/java/ca/tanasije/electronics_list/exception/NoSuchLaptopExistsException.java
@@ -0,0 +1,7 @@
+package ca.tanasije.electronics_list.exception;
+
+public class NoSuchLaptopExistsException extends RuntimeException {
+    public NoSuchLaptopExistsException(String message) {
+        super(message);
+    }
+}
