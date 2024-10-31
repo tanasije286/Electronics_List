cfc1a8d0dc5eed519302e80a4e52c978d9d1cee1
tanasije286 <tanasije286@gmail.com>
2024-10-31 15:16:26 -0400

    Updated GlobalExceptionHandler.java with exception handler lombok

--- src/main/java/ca/tanasije/electronics_list/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/ca/tanasije/electronics_list/exception_handler/GlobalExceptionHandler.java
@@ -1,14 +1,16 @@
 package ca.tanasije.electronics_list.exception_handler;
 
 import ca.tanasije.electronics_list.exception.NoSuchLaptopExistsException;
-import ca.tanasije.electronics_list.exception.error_response.ErrorResponse;
+import ca.tanasije.electronics_list.error_response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value= NoSuchLaptopExistsException.class)
     public ResponseEntity<ErrorResponse> NoSuchLaptopExistsException(NoSuchLaptopExistsException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
     }
