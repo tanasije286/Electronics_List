f36d16fa1dc62b75dd4485ab17bebd88c7000c9c
tanasije286 <tanasije286@gmail.com>
2024-10-31 14:48:18 -0400

    Updated lombok on ErrorResponse.java

--- src/main/java/ca/tanasije/electronics_list/exception/error_response/ErrorResponse.java
+++ src/main/java/ca/tanasije/electronics_list/exception/error_response/ErrorResponse.java
@@ -1,6 +1,13 @@
 package ca.tanasije.electronics_list.exception.error_response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
-    private Long statusCode;
+    private int statusCode;
     private String message;
 }
