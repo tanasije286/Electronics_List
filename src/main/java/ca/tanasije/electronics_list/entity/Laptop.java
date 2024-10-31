898da500fa7c44356c6bd48f66af4b2af8453ba1
tanasije286 <tanasije286@gmail.com>
2024-10-31 11:11:05 -0400

    Updated Laptop.java to have an additional constructor

--- src/main/java/ca/tanasije/electronics_list/entity/Laptop.java
+++ src/main/java/ca/tanasije/electronics_list/entity/Laptop.java
@@ -10,14 +10,13 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Laptop {
 
     @Id
@@ -26,26 +25,34 @@ public class Laptop {
 
     @NotBlank(message = "Label cannot be blank")
     @Size(max = 10, message = "Label can have up to 10 characters")
+    @NonNull
     private String laptopLabel;
 
     @NotBlank(message = "Brand cannot be blank")
     @Size(max = 50, message = "Brand can have up to 50 characters")
+    @NonNull
     private String laptopBrand;
 
     @NotBlank(message = "Serial Number cannot be blank")
     @Size(max = 100, message = "Serial Number can have up to 100 characters")
+    @NonNull
     private String laptopSerialNumber;
 
     @NotBlank(message = "Laptop Owner cannot be blank")
     @Size(max = 50, message = "Laptop Owner can have up to 50 characters")
+    @NonNull
     private String laptopOwner;
 
     @NotNull(message = "Location is required")
+    @NonNull
     private Location laptopLocation;
 
     @NotNull(message = "Primary use is required")
+    @NonNull
     private PrimaryUse laptopPrimaryUse;
 
     @NotNull(message = "Operating system is required")
+    @NonNull
     private OperatingSystem laptopOperatingSystem;
+
 }
