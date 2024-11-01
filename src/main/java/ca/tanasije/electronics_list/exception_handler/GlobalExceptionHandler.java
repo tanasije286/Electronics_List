7c96581b2cc01c33c23fc3b451b6329628b4de3a
tanasije286 <tanasije286@gmail.com>
2024-11-01 10:22:43 -0400

    Added exception if No such laptop exists

--- src/main/java/ca/tanasije/electronics_list/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/ca/tanasije/electronics_list/exception_handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ca.tanasije.electronics_list.exception_handler;
 
+import ca.tanasije.electronics_list.exception.LaptopAlreadyExistsException;
 import ca.tanasije.electronics_list.exception.NoSuchLaptopExistsException;
 import ca.tanasije.electronics_list.error_response.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -13,5 +14,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value= NoSuchLaptopExistsException.class)
     public ResponseEntity<ErrorResponse> NoSuchLaptopExistsException(NoSuchLaptopExistsException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(value= LaptopAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> LaptopAlreadyExistsException(LaptopAlreadyExistsException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage()));
     }
 }
