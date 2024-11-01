b645ef09298e5519e571feae05b08d2c79df5ca5
tanasije286 <tanasije286@gmail.com>
2024-11-01 09:32:05 -0400

    Added error message for laptop already existing

--- src/main/java/ca/tanasije/electronics_list/utility/error_message/ErrorMessage.java
+++ src/main/java/ca/tanasije/electronics_list/utility/error_message/ErrorMessage.java
@@ -2,6 +2,5 @@ package ca.tanasije.electronics_list.utility.error_message;
 
 public class ErrorMessage {
     public static final String ERROR_LAPTOP_NOT_FOUND = "Laptop does not exist with id: ";
-    public static final String ERROR_NO_HANDLER_MAPPING = "Resource not found: ";
-    public static final String ERROR_INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String ERROR_LAPTOP_ALREADY_FOUND = "Laptop already exists with id: ";
 }
